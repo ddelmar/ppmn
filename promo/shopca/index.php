d6ad7a9d367d539d287947ed27b5c29e0ade5358
David Delmar <ddelmar@where.com>
2013-09-26 13:27:57 -0400

    shop.ca jump page

---
+++ promo/shopca/index.php
@@ -0,0 +1,112 @@
+<!doctype>
+<html>
+
+<head>
+<meta charset="utf-8">
+
+<meta http-equiv="cache-control" content="no-cache" /> 
+<meta http-equiv="Content-Type" content="application/vnd.wap.xhtml+xml" /> 
+<meta name="HandheldFriendly" content="True" /> 
+<meta name="viewport" content="initial-scale=1.0; maximum-scale=1.0; user-scalable=0;" />
+
+<link rel="shortcut icon" href="https://www.paypalobjects.com/en_US/i/icon/pp_favicon_x.ico" />
+<link href="c.css" rel="stylesheet" type="text/css" />
+
+
+
+
+<title>PayPal | Shop.ca</title>
+
+</head>
+<body>
+<?php 
+/*
+	<div class="navParent">
+		<div class="navChild">
+			<ul>
+				<li class="contact">&copy; David Delmar. <a target="_blank" href="mailto:david@delmarsenties.com?subject=Regarding your weird book idea">Contact</a></li>
+			</ul>
+		</div><!-- /navChild -->
+	</div><!-- /navParent -->
+*/
+?>
+<div class="wrapper">
+<div class="container">
+<div class="pp-logo"><a href="/" title="PayPal Media Network">PayPal Media Network</a></div>
+	
+<h1>You have $25 off when you spend $75 of more at Shop.ca and pay with PayPal</h1>
+<p>
+	<a class="cat-link" href="http://shop.ca" title="Shop.ca">
+		<img class="shopca_coupon" src="shopca_coupon.png" />
+	</a>
+</p>
+
+<div class="copy-main">
+<p>
+To redeem this offer you won't need to enter any special code during checkout. Your offer is automatically applied when you spend $75 or more at Shop.ca pay with PayPal. It's that simple.</p><p>This is a limited time offer. Don’t let savings slip away. Shop today at <a href="http://shop.ca" title="shop.ca">shop.ca.</a> and save when you pay with PayPal. </p><p>Terms apply. Redeem by 10/14/2013</p><p>For an additional $25 off a $100 purchase, sign up for free at SHOP.CA, by visiting <a href="http://shop.ca" title="shop.ca">shop.ca.</a> 
+</p>
+</div><!-- -copy-main -->
+</div><!-- /container -->
+
+<div class="copy-terms">
+<p>Merchant Terms <br />Offer ends 10/14/2013 at 11:59 PM (EST). Cannot be combined with any other offer. Other restrictions may apply. </p>
+
+
+<p>PayPal Terms <br />You will receive a discount on your next purchase when you use PayPal and spend the minimum purchase amount at the participating website ("Offer"). Offer valid at the participating website from 12:01 AM EST on the start date through 11:59 PM EST on the expiration date stated on the Offer. Discount will be applied to merchandise after all applicable taxes have been added and will be subtracted from your total at checkout. To apply this Offer, you must a) create or have a Canadian PayPal account in good standing, b) receive an email from PayPal providing the Offer or view an Offer banner on PayPal.ca, c) complete an eligible purchase at a participating website using your PayPal account, and d) complete your purchase using PayPal no later than 11:59 PM EST on the Offer expiration date. The Offer excludes gift certificate payments and gift card purchases. Other exclusions may apply. If you seek a full refund for a purchase that applied the Offer, the full refund amount will be provided subject to the merchant's refund policy. If a partial refund is provided, you will receive the partial refund amount minus the Offer value. If an Offer is included in your refund, the Offer is still subject to its expiration date. Offer valid in Canada and cannot be combined with any other coupon, offer, discount or promotion code and may not be redeemed for cash. Void where prohibited. Offer may not be available when network connectivity between the merchant and PayPal is unavailable.
+</p>
+
+</div><!-- /terms -->
+</div><!-- /wrapper -->
+
+<script type="text/javascript" src="http://code.jquery.com/jquery-latest.min.js"></script>
+<script type="text/javascript">
+function getQuerystring(key, default_)
+{
+  if (default_==null) default_="";
+  key = key.replace(/[\[]/,"\\\[").replace(/[\]]/,"\\\]");
+	var key = key;
+  var regex = new RegExp("[\\?&]"+key+"=([^&#]*)");
+  var qs = regex.exec(window.location.href);
+  if(qs == null)
+    return default_;
+  else
+    return qs[1];
+    
+}
+$(document).ready(function(){
+	catnodash = getQuerystring('cat').replace("-"," ");
+	var cat = getQuerystring('cat');
+	$("a.cat-link").attr("href", "http://www.shop.ca/c/sc/" + cat);
+	$(".cat").html(catnodash);
+});
+
+
+</script>
+
+
+
+<?php /* -----------------------------------------------------
+Begin analytics code.
+-------------------------------------------------------------- */ ?>
+<!-- FPTI code Dated 2/21/2013 v1.1
+Copyright 1996-2013 eBay, Inc. All Rights Reserved -->
+<script src="https://www.paypalobjects.com/pa/js/min/pa_thirdparty.js"></script>
+<script>
+// Define Page Specific Variabless here.
+fpti.pgrp = 'main:ppmn:advertising::shopca';
+fpti.page = 'main:ppmn:advertising::shopca:::';
+fpti.ccpg = 'usa';
+fpti.lgin = 'out';
+fpti.rsta = 'us-en';
+fptiserver = '//t.paypal.com/ts';
+// End Page Specific Variabless.
+
+// DO NOT ALTER ANYTHING BELOW THIS LINE FOR FPTI!
+if(typeof PAYPAL.analytics != 'undefined'){
+	PAYPAL.pta = PAYPAL.analytics.setup();
+}
+</script>
+<noscript><img src="https://tracking.qa.paypal.com/ts?nojs=1&amp;pgrp=main%3Appmn%3Aadvertising%3A%3Aslides" alt="" height="1" width="1" border="0" /></noscript>
+<!-- End FPTI code -->
+</body>
+</html>
