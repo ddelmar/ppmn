1458fe0b6e1922d138e0459d6a3ee8e11e2a214f
David Delmar <ddelmar@where.com>
2013-10-04 13:06:23 -0400

    added io

---
+++ io/index.php
@@ -0,0 +1,75 @@
+<?php require_once('../_/inc/doctype-html.php'); ?>
+	<head>
+		<?php require_once('../_/inc/common-head.php'); ?>
+		<title>Terms and Conditions | PayPal Media Network</title>
+		<meta name="description" content="" />
+	</head>
+	<body>
+		<?php require_once('../_/inc/header.php'); ?>
+		<div class="main">
+			<?php require_once('../_/inc/second-pp-logo.php'); ?>
+
+
+			<h1>Proposal Guidelines</h1>
+			<h2>Terms and Conditions</h2>
+			<p>PayPal Media Network (PPMN) adheres to standard <a href="http://www.iab.net/guidelines/508676/tscs3" title="IAB Terms and Conditions">V3 IAB Terms & Conditions</a> (including NET 30 payment and cancellation terms of 30-days (spons.) and 14 days. Please also consult our complete <a href="/terms-and-conditions" title="PPMN Terms and Conditions">Terms and Conditions</a> as well as our <a href="/specs" title="Ad Specs">Ad Specs.</a></p>
+			
+			
+			<h2>Inventory Projections, SOV, and availability</h2>
+			<p>
+			&bull; PPMN is a mobile ad network; all campaign impression availability and estimated SOV are at time of proposal and subject to change based on availability 					
+			<br />&bull; Aforementioned rates apply to this proposal only and are valid for 60 days.					
+			<br />&bull; Exclusive sponsorships and PlaceIQ placements are based on availability; sponsorships will preempt any ROS placements.  					
+			<br />&bull; All bonus impressions are estimated but not guaranteed, subject to inventory, and are not guaranteed to be delivered evenly on daily/weekly/monthly basis.					
+			<br />&bull; Paid placements always override bonus placements; PPMN does not make good on preemption or underdelivery of bonus items.
+			</p>
+			
+			
+			<h2>Rich media, third-party ad serving</h2>		
+			<p>		
+		<br />&bull; PPMN is a mobile ad network; all campaign impression availability and estimated SOV are at time of proposal and subject to change based on availability  If leveraging PayPal's proprietary location data and geo-fencing, PPMN must serve ads through proprietary ad server					
+		<br />&bull; No network-paid serving fees. Any serving fees must be paid by advertiser directly					
+		<br />&bull; Clients using 3rd-party ad servers for billable reporting must provide PPMN grant reporting access to traffic@paypal.com at launch of campaign. Flight must be >3 days to bill off 3rd party data.					
+		<br />&bull; PPMN works with most major 3rd parties, however, all 3rd parties are subject to testing and certification.  Atlas is not an approved 3rd party vendor.	
+			</p>
+			
+			<h2>Ad specs and custom solutions</h2>				
+			<p>&bull; Surveys:  All surveys and survey vendors must be approved prior to running.  Recruitment must be served via bonus impressions and in-banner recruitment only. PPMN cannot guarantee a number of survey completions during the control and exposed phases. <br />&bull; Verification:  3rd Party Verfication pixels can be implemented but all verification pixel fees and costs must be paid by the advertiser/agency, PPMN will not be responsible for any costs associated with the implementation of verification pixels.  PPMN will also strive to make any updates, as needed, based upon the verification reportin,g however, due to the nature of a network, some items are outside of PPMN's control and cannot be updated. <br />&bull; Converstion Tracking:  PPMN strives to meet campaign performance metrics but due to the nature of conversion tracking for mobile, optimizations will be based upon banner performance, not conversions.	<br />&bull; Required Assets:  All creatives must be delivered to PPMN within the agreed upon timeframe to guarantee timely launch of campaign. Lead-time may increase for custom products and integrations. Please check with your dedicated Sales Rep for more information.	<br />&bull; Real Time Bidding (RTB):  Placements are based upon a daily budget goal, not an impression goal.  The impressions listed on the IO are for estimate purposes only.  More or less impressions could be delivered depending on the winning bid amount.  3rd party tracking will be implemented, however the contract is based upon spend, which is not tracked in a 3rd party reporting, therefore, billing will be based upon the spend recorded from PPMN numbers.	
+			</p>
+							
+			<h2>Demand Generation - Offers </h2>
+			<p>					
+&bull; Funder Account:  Prior to campaign launch a Funder Account must be designated.  The Funder Account will be used for settling the offer cost.  All funding is subject to pre-approval.					
+<br />&bull; Reporting:  PPMN will generate an Offer Settlement Report (OSR) that will provide the details of the transactions incurred based upon the offer.  Billing will be determined based upon the total redemptions and any occurring media costs.
+</p>
+
+		</div><!-- .main -->
+		<?php require_once('../_/inc/modal-contact-form.php'); ?>
+		<?php require_once('../_/inc/common-foot.php'); ?>
+
+
+		<?php /* -----------------------------------------------------
+		Begin analytics code.
+		-------------------------------------------------------------- */ ?>
+		<!-- FPTI code Dated 2/21/2013 v1.1
+		Copyright 1996-2013 eBay, Inc. All Rights Reserved -->
+		<script src="https://www.paypalobjects.com/pa/js/min/pa_thirdparty.js"></script>
+		<script>
+		// Define Page Specific Variabless here.
+		fpti.pgrp = 'main:ppmn:advertising::termsandconditions';
+		fpti.page = 'main:ppmn:advertising::termsandconditions:::';
+		fpti.ccpg = 'usa';
+		fpti.lgin = 'out';
+		fpti.rsta = 'us-en';
+		fptiserver = '//t.paypal.com/ts';
+		// End Page Specific Variabless.
+
+		// DO NOT ALTER ANYTHING BELOW THIS LINE FOR FPTI!
+		if(typeof PAYPAL.analytics != 'undefined'){
+			PAYPAL.pta = PAYPAL.analytics.setup();
+		}
+		</script>
+		<noscript><img src="https://tracking.qa.paypal.com/ts?nojs=1&amp;pgrp=main%3Appmn%3Aadvertising%3A%3Atermsandconditions" alt="" height="1" width="1" border="0" /></noscript>
+		<!-- End FPTI code -->
+	</body>
+</html>
