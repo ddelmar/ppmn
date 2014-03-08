7cd9505ef8ae11d4305ddc43097b9f851e953f0f
David Delmar <ddelmar@where.com>
2014-03-07 21:11:21 -0500

    added boost LPs

---
+++ promo/boost/buyerprotection/index.php
@@ -0,0 +1,35 @@
+
+<!DOCTYPE HTML>
+<html>
+<head>
+<link rel="stylesheet" href="/promo/boost/c/boost.css" />
+<meta charset="utf-8">
+<title>PayPal - Buyer Protection</title>
+</head>
+
+<body>
+<div id="container">
+	<div id="content">
+		<div id="shieldImage">
+			<img src="/promo/boost/i/protection.png" alt="" />
+		</div>
+		<div id="text" style="top: 250px;">
+		<p id="title">Buyer Protection</p>
+		<p id="body">If an eligible item that you’ve purchased online, eBay or otherwise, doesn’t arrive or doesn’t match the seller’s description, we will look into it. If it's discovered that something is wrong, our Purchase Protection will reimburse you for the full purchase price of the item plus shipping costs.
+			<br />
+			<br />
+			For more information, <a href="https://www.paypal.com/webapps/mpp/ua/useragreement-full?country.x=US&locale.x=en_US#13" target="_blank">see user agreement</a>
+		</p>
+		</div>
+		<div id="logo">
+		<img src="/promo/boost/i/PPLogo.png" alt="" />
+		</div>
+	
+	</div>
+
+</div>
+
+
+</body>
+
+</html>
