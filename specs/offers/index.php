4fc5c85f64cedd41c9e2eacaabf7fbe1ee105061
David Delmar <ddelmar@where.com>
2014-04-08 10:02:21 -0400

    added CLO specs

---
+++ specs/offers/index.php
@@ -0,0 +1,87 @@
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <meta name="viewport" content="width=320">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PayPal</title>
+
+	<style>
+	body {
+		margin:auto;
+		width:55%;
+		background-color:#dedede;
+		}
+	.header {
+		font-family:Arial, Helvetica, sans-serif; 
+		font-size:24px; 
+		line-height:auto; 
+		text-decoration:none; 
+		color:#666666;
+		padding-top:20px;
+		}
+	.subinfo {
+		font-family:Arial, Helvetica, sans-serif; 
+		font-size:13px; 
+		line-height:20px; 
+		text-decoration:none; 
+		color:#666666;
+		}
+	#images {
+		float:right;
+		width:60%;
+		max-width:500px;
+		padding:15px;
+		margin-right:3%;
+		}
+	#images img {
+		width:100%;
+		}
+	#copy {
+		width:33%;
+		margin-right:25%;
+		}
+	</style>
+
+</head>
+
+<body>
+	
+	<div id="images">
+		<img src="http://where-spotlight.s3.amazonaws.com/CLOs/_Templates/image-guide/i/clo-image-examples.png">
+	</div><!-- end images -->
+	
+	<div id="copy">
+
+	<div class="header">Products occupy same physical space</div>
+	<div class="subinfo">
+	
+	<ul>
+		<li>Camera angle should be either "overhead" (Example 1) or "tabletop" (Examples 2 &amp; 3)</li>
+		<li>Highlights and shadows should be consistent and come from a single source.</li>
+		<li>Drop shadows should be realistic, not default Photoshop add ons</li>
+	</div>
+
+	<div class="header">Background color #F7F7F7</div>
+	<div class="subinfo">
+		<ul>
+		<li>Solid background or ambient background that convincingly blends into #F7F7F7</li></div>
+
+	<div class="header">600x300 px</div>
+	<div class="subinfo"><ul>
+		<li>Size is optimized for mobile viewing, maximum size 600x600px</li></div>
+
+	<div class="header">Logo </div>
+	<div class="subinfo">
+	Merchant should:
+	<ul>
+		<li>include logo</li>
+		<li>have space in image to add in logo</li>
+		<li>opt out of including a logo</li>
+	</div>
+	
+	</div><!-- end copy -->
+
+
+</body></html>
